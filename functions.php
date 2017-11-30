b75fdc06e813ac3b01ade8ae8070fc278789268c
Hunter m <Googolaire@github.com>
2017-11-30 00:50:22 -0800

    changed the directory inforamtion

--- functions.php
+++ functions.php
@@ -1,8 +1,8 @@
 function my_assets() {
 	wp_enqueue_style( 'theme-style', get_stylesheet_uri(), array( 'responsive-screen.css' ) );
-	wp_enqueue_style( 'responsive-screen', get_stylesheet_directory_uri() . 'Some-Thing-new/css/responsive-screen.css' );
-	wp_enqueue_style( 'screen', get_stylesheet_directory_uri() . 'Some-Thing-new/css/screen.css' );
-	wp_enqueue_style( 'vendor', get_stylesheet_directory_uri() . 'Some-Thing-new/css/vendor.css' );
+	wp_enqueue_style( 'responsive-screen', get_stylesheet_directory_uri() . '/Some-Thing-new/css/responsive-screen.css' );
+	wp_enqueue_style( 'screen', get_stylesheet_directory_uri() . '/Some-Thing-new/css/screen.css' );
+	wp_enqueue_style( 'vendor', get_stylesheet_directory_uri() . '/Some-Thing-new/css/vendor.css' );
 
 	wp_enqueue_script( 'owl-carousel', get_stylesheet_directory_uri() . '/owl.carousel.js', array( 'jquery' ) );
 	wp_enqueue_script( 'theme-scripts', get_stylesheet_directory_uri() . '/website-scripts.js', array( 'owl-carousel', 'jquery' ), '1.0', true );
