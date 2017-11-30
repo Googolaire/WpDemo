c9e11dda1463155521a112d3ca7b994b0e14af84
Hunter m <Googolaire@github.com>
2017-11-30 00:41:17 -0800

    Modified lines 3 - 5

--- functions.php
+++ functions.php
@@ -1,8 +1,8 @@
 function my_assets() {
 	wp_enqueue_style( 'theme-style', get_stylesheet_uri(), array( 'responsive-screen.css' ) );
-	wp_enqueue_style( 'reset', get_stylesheet_directory_uri() . '/css/responsive-screen.css' );
-	wp_enqueue_style( 'reset', get_stylesheet_directory_uri() . '/css/screen.css' );
-	wp_enqueue_style( 'reset', get_stylesheet_directory_uri() . '/css/vendor.css' );
+	wp_enqueue_style( 'responsive-screen', get_stylesheet_directory_uri() . '/responsive-screen.css' );
+	wp_enqueue_style( 'screen', get_stylesheet_directory_uri() . '/screen.css' );
+	wp_enqueue_style( 'vendor', get_stylesheet_directory_uri() . '/vendor.css' );
 
 	wp_enqueue_script( 'owl-carousel', get_stylesheet_directory_uri() . '/owl.carousel.js', array( 'jquery' ) );
 	wp_enqueue_script( 'theme-scripts', get_stylesheet_directory_uri() . '/website-scripts.js', array( 'owl-carousel', 'jquery' ), '1.0', true );
